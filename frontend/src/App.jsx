0a6c30b40e1dcbf802458889d608a3fd4bbde1a0
Arpandobariya15 <work.arpan8980@gmail.com>
2024-12-24 11:31:04 +0530

    final

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React,{useState ,useEffect } from 'react';
 import "../src/components/EventList.css"
 import EventForm from './components/EventForm';
 import axios from 'axios';
+import EventList from './components/EventList';
 
 const App = () => {
   const [events, setEvents] = useState([]);
@@ -17,6 +18,54 @@ const App = () => {
   const handleEventAdd = (newEvent) => {
 		setEvents([...events, newEvent]);
 	};
+  // delete event
+  const handleEventDelete = (id) => {
+		axios.delete(`http://localhost:5000/api/event/${id}`).then(() =>setEvents(events.filter(event => event._id !== id)))
+			.catch(error => console.error(error));
+	};
+  // handle toggle
+  const handleToggleReminder = (eventId) => {
+		const selectedEvent =
+			events.find(event => event._id === eventId);
+		const updatedEvent =
+		{
+			...selectedEvent,
+			reminder: !selectedEvent.reminder
+		};
+		axios.put(`http://localhost:5000/api/event/${eventId}`, updatedEvent)
+			.then(res => {
+				// If the update is successful, update the events in the state
+				const updatedEvents = events.map(event =>
+					event._id === eventId ? updatedEvent : event
+				);
+				setEvents(updatedEvents);
+			})
+			.catch(
+				error =>
+					console.error(`Error updating reminder status for
+				event with ID ${eventId}:`, error));
+	};
+	// Update the event in the database
+  const onEventEdit = (eventId, updatedData) => {
+		axios.put(`http://localhost:5000/api/event/${eventId}`, updatedData)
+			.then(res => {
+				// If the update is successful, update the events in the state
+				const updatedEvents = events.map(event =>
+					event._id ===
+						eventId ?
+						{ ...event, ...updatedData } : event
+				);
+				setEvents(updatedEvents);
+			})
+			.catch(
+				error =>
+					console.error(`Error updating event with
+						ID ${eventId}:`, error)
+			);
+	};
+
+
+
 
 
 
@@ -29,12 +78,12 @@ const App = () => {
       </h1>
       <h2>Event Management App</h2>
       <EventForm  onEventAdd={handleEventAdd} />
-      {/* <EventList
+      <EventList
         events={events}
         onEventDelete={handleEventDelete}
         onToggleReminder={handleToggleReminder}
         onEventEdit={onEventEdit}
-      /> */}
+      />
     </div>
 
   );
